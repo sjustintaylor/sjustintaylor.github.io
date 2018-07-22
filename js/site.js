eb3e6b6380686df67ce8317247c396991e9b3eec
Justin Taylor <s.justin.taylor@gmail.com>
2018-07-22 13:03:08 +1000

    Final touches before publishing

--- js/site.js
+++
@@ -1,8 +0,0 @@
-var responsiveToggle = function(){
-    $('#mobile-menu').on('click',function(){
-      $('#mn-menu-actual').slideToggle();
-    });
-}
-$( document ).ready(function(){
-  responsiveToggle();
-});
