4b39117eb6380a1ca7e0af90ca72b0ffca6ad8b6
Justin Taylor <s.justin.taylor@gmail.com>
2020-04-21 21:56:07 +1000

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git branch -D gh-pages
+git checkout -b gh-pages
+
+zola build
+
+rm -r content/ themes/ .browserslistrc .editorconfig .eslintrc.js .gitignore config.toml 
+mv public/* .
+rm -r public
+
+git add .
+git commit -am "latest design"
+git push -f origin gh-pages:master
+git checkout zola-source
