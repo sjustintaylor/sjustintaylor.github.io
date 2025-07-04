68bc76a8f1ac55e57c0a7503fb0d272ddbff13e6
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-07-04 18:13:54 +1000

    feat: checkpoint

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,22 +1,51 @@
-import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { getPath } from "@/utils/getPath";
 import getSortedPosts from "@/utils/getSortedPosts";
 import { SITE } from "@/config";
 
+import { Feed } from "feed";
+
 export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
-  return rss({
+
+  const feed = new Feed({
     title: SITE.title,
     description: SITE.desc,
-    site: SITE.website,
-    items: sortedPosts.map(({ data, id, filePath, rendered }) => ({
-      link: getPath(id, filePath),
+    id: SITE.website,
+    link: SITE.website,
+    language: "en",
+    favicon: `${SITE.website}/favicon.ico`,
+    copyright: `All rights reserved ${new Date().getFullYear()}`,
+    feedLinks: {
+      atom: `${SITE.website}/feed.xml`,
+    },
+    author: {
+      name: "Justin Taylor",
+    },
+  });
+
+  sortedPosts.forEach(({ data, id, filePath, rendered }) => {
+    const url = getPath(id, filePath);
+
+    feed.addItem({
+      link: url,
       title: data.title,
       description: data.description,
-      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
       content: rendered?.html,
-    })),
+      id: url,
+      author: [
+        {
+          name: data.author || "Justin Taylor",
+        },
+      ],
+      date: new Date(data.modDatetime ?? data.pubDatetime),
+    });
+  });
+
+  return new Response(feed.atom1(), {
+    headers: {
+      "Content-Type": "application/atom+xml",
+    },
   });
 }
